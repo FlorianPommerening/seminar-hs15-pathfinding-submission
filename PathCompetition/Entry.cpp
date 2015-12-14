51d77a0d3aef8e42cfcaf57ee6da855ae0bb6253
Florian Pommerening <florian.pommerening@unibas.ch>
2015-12-14 23:43:27 +0100

    Preprocess data and store to temporary file

--- PathCompetition/Entry.cpp
+++ PathCompetition/Entry.cpp
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <cmath>
+#include <fstream>
 
 
 using namespace std;
@@ -22,21 +23,32 @@ const char *GetName() {
 
 
 void PreprocessMap(vector<bool> &bits, int width, int height, const char *filename) {
-    printf("Not writing to file '%s'\n", filename);
+    // Note: this is a memory leak, but the infrastructure code gives us no choice.
+    MapInfo map_info;
+    map_info.map = bits;
+    map_info.width = width;
+    map_info.height = height;
+    detect_rooms(map_info);
+    detect_gates(map_info);
+    draw_rooms("rooms.png", map_info);
+
+    printf("Writing to file '%s'\n", filename);
+    ofstream map_file;
+    map_file.open (filename);
+    map_file << map_info;
+    map_file.close();
 }
 
 
 void *PrepareForSearch(vector<bool> &bits, int w, int h, const char *filename) {
-    printf("Not reading from file '%s'\n", filename);
-
     // Note: this is a memory leak, but the infrastructure code gives us no choice.
     MapInfo *map_info = new MapInfo;
-    map_info->map = bits;
-    map_info->width = w;
-    map_info->height = h;
-    detect_rooms(*map_info);
-    detect_gates(*map_info);
-    draw_rooms("rooms.png", *map_info);
+
+    printf("Reading from file '%s'\n", filename);
+    ifstream map_file(filename);
+    map_file >> (*map_info);
+    map_file.close();
+
     return map_info;
 }
 
---
+++ PathCompetition/map_info.cpp
@@ -0,0 +1,103 @@
+#include "map_info.h"
+
+using namespace std;
+
+ostream &operator<<(ostream &os, const Exit &e) {
+    os << e.id << " "
+       << e.room_id << " "
+       << e.location.x << " "
+       << e.location.y << " "
+       << e.successors.size() << endl;
+    for (int i = 0; i < e.successors.size(); ++i) {
+        os << e.successors[i];
+    }
+}
+
+istream &operator>>(istream &is, Exit &e) {
+    int num_succ;
+    is >> e.id >> e.room_id >> e.location.x >> e.location.y >> num_succ;
+    e.successors.resize(num_succ);
+    for (int i = 0; i < num_succ; ++i) {
+        is >> e.successors[i];
+    }
+}
+
+ostream &operator<<(ostream &os, const ExitSuccesor &s) {
+    os << s.succ_id << " " << s.distance << endl;
+}
+
+istream &operator>>(istream &is, ExitSuccesor &s) {
+    is >> s.succ_id >> s.distance;
+}
+
+ostream &operator<<(ostream& os, const MapInfo &m) {
+    os << m.width << " " << m.height;
+    for (int i = 0; i < m.map.size(); ++i) {
+        if (i % m.width == 0)
+            os << endl;
+        os << (m.map[i]? ".":"@");
+    }
+    os << endl;
+
+    os << m.num_rooms;
+    for (int i = 0; i < m.rooms.size(); ++i) {
+        if (i % m.width == 0)
+            os << endl;
+        os << m.rooms[i] << " ";
+    }
+    os << endl;
+
+    os << m.room_exits.size() << endl;
+    for (const auto &r_exits : m.room_exits) {
+        os << r_exits.size() << " ";
+        for (int exit_id : r_exits) {
+            os << exit_id << " ";
+        }
+        os << endl;
+    }
+    os << endl;
+
+    os << m.exits.size() << endl;
+    for (const auto &e : m.exits) {
+        os << e;
+    }
+    os << endl;
+    return os;
+}
+
+istream &operator>>(istream& is, MapInfo &m) {
+    is >> m.width >> m.height;
+    m.map.resize(m.width * m.height);
+    for (int i = 0; i < m.map.size(); ++i) {
+        char map_elem;
+        is >> map_elem;
+        m.map[i] = (map_elem == '.');
+    }
+
+    is >> m.num_rooms;
+    m.rooms.resize(m.width * m.height);
+    for (int i = 0; i < m.rooms.size(); ++i) {
+        is >> m.rooms[i];
+    }
+
+    int num_room_exits;
+    is >> num_room_exits;
+    m.room_exits.resize(num_room_exits);
+    for (int i = 0; i < num_room_exits; ++i) {
+        int num_room_exits_i;
+        is >> num_room_exits_i;
+        m.room_exits[i].resize(num_room_exits_i);
+        for (int j = 0; j < num_room_exits_i; ++j) {
+            is >> m.room_exits[i][j];
+        }
+    }
+
+    int num_exits;
+    is >> num_exits;
+    m.exits.resize(num_exits);
+    for (int i = 0; i < num_room_exits; ++i) {
+        is >> m.exits[i];
+    }
+
+    return is;
+}
